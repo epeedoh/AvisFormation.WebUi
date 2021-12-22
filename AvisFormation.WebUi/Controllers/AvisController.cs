564b0e7bea37247552991e702491f99a56a013e1
edoh.epe <edoh.epe@ixperta-ims.com>
2021-12-22 17:34:38 +0000

    Ajoutez des fichiers projet.

---
+++ AvisFormation.WebUi/Controllers/AvisController.cs
@@ -0,0 +1,69 @@
+﻿using AvisFormation.Data;
+using AvisFormation.WebUi.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AvisFormation.WebUi.Controllers
+{
+    public class AvisController : Controller
+    {
+        // GET: Avis
+        public ActionResult LaisserUnAvis(string nomSeo)
+        {
+            var vm = new LaisserUnAvisViewModel();
+            vm.NomSeo = nomSeo;
+
+            using (var context = new DbEntities())
+            {
+                var formationEntity = context.Formation.FirstOrDefault(f => f.NomSeo == nomSeo);
+
+                if (formationEntity == null)
+                    return RedirectToAction("Accueil", "Home");
+
+                vm.FormationName = formationEntity.Nom;
+            }
+
+                return View(vm);
+        }
+
+        public ActionResult SaveComment(string commentaire, string nom, string note, string nomSeo)
+        {
+
+            Avis nouvelAvis = new Avis()
+            {
+                DateAvis = DateTime.Now,
+                Description = commentaire,
+                //IdFormation = ,
+                Nom = nom
+            };
+
+            double bNote = 0;
+            if(!double.TryParse(note,NumberStyles.Any,CultureInfo.InvariantCulture, out bNote))
+            {
+                throw new Exception("Impossible de parser la note" + note);
+            }
+
+            nouvelAvis.Note = bNote;
+
+            using(var context = new DbEntities())
+            {
+                var formationEntity = context.Formation.FirstOrDefault(f => f.NomSeo == nomSeo);
+
+                if (formationEntity == null)
+                    return RedirectToAction("Accueil", "Home");
+
+                nouvelAvis.IdFormation = formationEntity.Id;
+
+                context.Avis.Add(nouvelAvis);
+                context.SaveChanges();
+            }
+
+            return View();
+        }
+
+    }
+}
---
+++ AvisFormation.WebUi/Controllers/FormationController.cs
@@ -0,0 +1,88 @@
+﻿using AvisFormation.Data;
+using AvisFormation.WebUi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AvisFormation.WebUi.Controllers
+{
+    public class FormationController : Controller
+    {
+        // GET: Formation
+        public ActionResult ToutesLesFormations()
+        {
+
+            List<Formation> listFormations = new List<Formation>();
+
+            using(var context = new DbEntities())
+            {
+                 listFormations = context.Formation.ToList();
+            }
+
+            return View(listFormations);
+        }
+
+        public ActionResult DetailsFormation(string nomSeo)
+        {
+            var vm = new FormationAvecAvisViewModel();
+
+            using(var context = new DbEntities())
+            {
+                var formationEntity = context.Formation.Where(f => f.NomSeo == nomSeo).FirstOrDefault();
+
+                if(formationEntity == null)
+                  return RedirectToAction("Accueil", "Home");
+
+                vm.FormationNom = formationEntity.Nom;
+                vm.FormationDescription = formationEntity.Description;
+                vm.FormationNomSeo = formationEntity.NomSeo;
+                vm.FormationUrl = formationEntity.Url;
+                vm.Note = formationEntity.Avis.Average(a => a.Note);
+                vm.NombreAvis = formationEntity.Avis.Count;
+                vm.Avis = formationEntity.Avis.ToList();
+            }
+
+            return View(vm);
+
+        }
+
+        public void InsertDataToDatabase()
+        {
+
+            var newFormation = new Formation()
+            {
+                Nom = "Epe",
+                Description = "Test"
+            };
+
+            using(var context = new DbEntities())
+            {
+
+                context.Formation.Add(newFormation);
+                context.SaveChanges();
+
+            }
+
+        }
+
+
+        public void UpdateDataToDatabase()
+        {
+
+            using(var context = new DbEntities())
+            {
+                var entityFormation = context.Formation.FirstOrDefault(f => f.NomSeo == "dd");
+
+                if(entityFormation != null)
+                {
+                    entityFormation.Nom = "toto";
+                }
+                context.SaveChanges();
+            }
+
+        }
+
+    }
+}
